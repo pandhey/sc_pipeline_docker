68e917227f6e823c521cb622a49a949f5dc194d7
pandhey <42649146+pandhey@users.noreply.github.com>
2019-05-08 12:56:43 -0500

    Update intro_single.R

--- templates/intro_single.R
+++ templates/intro_single.R
@@ -1,4 +1,9 @@
+dataFolder <- "{{ PathToAligned }}"
 
-dataFolder <- list.dirs("{{ PathToAligned }}/{{ SampleId }}/outs/filtered_gene_bc_matrices", recursive=F)
-fdata <- Read10X(dataFolder)
-whole <- CreateSeuratObject(raw.data=fdata, project = "{{ SampleName }}")
+dataFolders <- list()
+dataFolders[[1]] <- paste(dataFolder,"/{{ SampleId }}/outs/filtered_gene_bc_matrices", recursive=F)
+
+fdata <- list()
+fdata[[1]] <- Read10X(dataFolders[[1]])
+
+whole <- CreateSeuratObject(counts=fdata[[1]], project = "{{ SampleName }}")
