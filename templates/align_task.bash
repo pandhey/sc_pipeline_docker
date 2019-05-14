951c2401f07dcaa8ff5d4275c9af34b286e39b78
pandhey <42649146+pandhey@users.noreply.github.com>
2019-05-14 14:08:02 -0500

    Update align_task.bash

--- templates/align_task.bash
+++ templates/align_task.bash
@@ -9,7 +9,7 @@ cd {{ PathToAligned }}
 LSF_DOCKER_PRESERVE_ENVIRONMENT=false bsub -q research-hpc \
         -J ${NAME}.stg -n 8 -M 64000000 -o ${NAME}.string.out \
         -e ${NAME}.string.err -R 'select[mem>64000] rusage[mem=64000]' \
-        -a "docker(fbrundu/cellranger)" /bin/bash -c "cellranger count --id=$NAME \
+        -a "docker(psandhey/cellranger)" /bin/bash -c "cellranger count --id=$NAME \
         --transcriptome={{ Genome }} \
         --fastqs={{ PathToFastq }}/${NAME} \
         --nosecondary \
