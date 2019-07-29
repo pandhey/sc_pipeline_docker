2f203766d66d421bb2148717d9e603a0b8150511
pandhey <42649146+pandhey@users.noreply.github.com>
2019-07-29 11:25:03 -0500

    Update markers.R

--- templates/markers.R
+++ templates/markers.R
@@ -1,5 +1,5 @@
 whole.markers <- FindAllMarkers(object = whole,
                                 only.pos = TRUE,
                                 min.pct = 0.10,
-                                thresh.use = 0.10)
+                                logfc.threshold = 0.10)
 write.table(whole.markers, "markers.tsv", sep="\t", quote=F, row.names=F)
