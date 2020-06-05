83eeb44af45ce342ef9933fa7b406d68b1be70f4
pandhey <42649146+pandhey@users.noreply.github.com>
2020-06-04 20:42:11 -0500

    Update align_task.bash

--- templates/align_task.bash
+++ templates/align_task.bash
@@ -2,13 +2,13 @@
 
 NAME=$1
 
-
+export LSF_DOCKER_VOLUMES="/storage1/fs1/martyomov/Active/:/storage1/fs1/martyomov/Active/  /scratch1/fs1/martyomov:/scratch1/fs1/martyomov /home/IDC-ID-63498:/home/IDC-ID-63498"
 cd {{ PathToAligned }}
 
 
-LSF_DOCKER_PRESERVE_ENVIRONMENT=false bsub -q research-hpc \
-        -J ${NAME}.stg -n 8 -M 64000000 -o ${NAME}.string.out \
-        -e ${NAME}.string.err -R 'select[mem>64000] rusage[mem=64000]' \
+LSF_DOCKER_PRESERVE_ENVIRONMENT=false bsub -q general -G compute-martyomov \
+        -J ${NAME}.stg -n 8 -M 64GB -o ${NAME}.string.out \
+        -e ${NAME}.string.err -R 'select[mem>64MB] rusage[mem=64GB] span[hosts=1]' \
         -a "docker(psandhey/cellranger)" /bin/bash -c "cellranger count --id=$NAME \
         --transcriptome={{ Genome }} \
         --fastqs={{ PathToFastq }}/${NAME} \
